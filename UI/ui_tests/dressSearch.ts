009dc343a823bccb555112922b3acd9f84269674
davidwagih <78867146+david-wagih@users.noreply.github.com>
2023-07-16 19:49:29 +0300

    add the dress search test

---
+++ UI/ui_tests/dressSearch.ts
@@ -0,0 +1,52 @@
+import { NightwatchTests } from "nightwatch";
+
+const home: NightwatchTests = {
+  "My Store Title Test": () => {
+    browser
+      .url("http://automationpractice.multiformis.com/index.php")
+      .waitForElementVisible("body")
+      .assert.titleContains("My Store");
+  },
+
+  "Search for a dress": () => {
+    browser
+      .setValue("#search_query_top", "dress")
+      .click("#searchbox > button")
+      .waitForElementVisible("#product_list .ajax_block_product")
+      .elements(
+        "css selector",
+        "#product_list .ajax_block_product",
+        (result: any) => {
+          browser.assert.equal(result.value.length, 7);
+          result.value.forEach((element: any) => {
+            browser.elementIdElement(
+              element["element-6066-11e4-a52e-4f735466cecf"],
+              "css selector",
+              ".ajax_block_product .product-container .right-block",
+              (rightBlock: any) => {
+                browser.elementIdElement(
+                  rightBlock["elementId"],
+                  "css selector",
+                  "h5 a",
+                  (anchor: any) => {
+                    browser.elementIdText(
+                      anchor["elementId"],
+                      (anchorText: any) => {
+                        const productName = anchorText.value.toLowerCase();
+                        browser.assert.ok(
+                          productName.includes("dress"),
+                          `Product name "${productName}" contains the word "dress"`
+                        );
+                      }
+                    );
+                  }
+                );
+              }
+            );
+          });
+        }
+      );
+  },
+};
+
+export default home;
